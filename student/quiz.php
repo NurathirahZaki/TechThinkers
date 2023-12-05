5b2720da1d45cb27d69f6956ccf1deced7b499ae
Farah Nabilah <farah.nabilah@graduate.utm.my>
2023-12-05 11:26:58 +0800

    Add files via upload

---
+++ student/quiz.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Page</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to the style.css file -->
+    <!-- Other necessary meta tags, scripts, etc. -->
+</head>
+<body>
+    <!-- Content in your quiz.php file -->
+    <div class="course-section-header d-flex" data-for="section_title" data-id="76412" data-number="9" id="yui_3_18_1_1_1701690511671_198">
+        <!-- Example of a quiz link embedded within the course section header -->
+        <h3>Quiz Section</h3>
+        <p>Below is a link to a quiz:</p>
+        <a href="https://quizizz.com/join/quiz/60af1c6baa5e3d001c7a70ff/start?studentShare=true" class="AWS 1">Quiz Link</a>
+    </div>
+    <!-- Other content in your quiz.php file -->
+    <div class="course-section-header d-flex" data-for="section_title" data-id="76412" data-number="9" id="yui_3_18_1_1_1701690511671_198">
+        <!-- Example of a quiz link embedded within the course section header -->
+        <h3>Quiz Section</h3>
+        <p>Below is a link to a quiz:</p>
+        <a href="https://quizizz.com/join/quiz/5ffd820da290ed001b496d61/start?studentShare=true" class="AWS 2">Quiz Link</a>
+    </div>
+    <div class="course-section-header d-flex" data-for="section_title" data-id="76412" data-number="9" id="yui_3_18_1_1_1701690511671_198">
+        <!-- Example of a quiz link embedded within the course section header -->
+        <h3>Quiz Section</h3>
+        <p>Below is a link to a quiz:</p>
+        <a href="https://quizizz.com/join/quiz/637589808191cb001e4938ea/start?studentShare=true" class="AWS 3">Quiz Link</a>
+    </div>
+</body>
+</html>
